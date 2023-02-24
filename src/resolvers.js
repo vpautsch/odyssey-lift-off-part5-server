790f82ea53ad5eb516050f3139452245c0d71b47
vpautsch <vpautsch@spins.com>
2023-02-24 15:53:54 -0600

    Deprecate length field in Module and Track types and replace it with durationInSeconds field.

--- src/resolvers.js
+++ src/resolvers.js
@@ -41,9 +41,14 @@ const resolvers = {
       return dataSources.trackAPI.getAuthor(authorId);
     },
 
+    durationInSeconds: ({ length }) => length,
+
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+  },
+  Module: {
+    durationInSeconds: ({ length }) => length,
   },
 };
 
